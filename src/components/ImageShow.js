8d7844fb87ef9fea489670b2bef70288230a8dc4
srijal <srijal@planittesting.com>
2022-12-13 20:05:37 +1000

    initial project setup with components

---
+++ src/components/ImageShow.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ImageList from './ImageList'
+function ImageShow() {
+    return (
+        <div>This is ImageShow
+            <ImageList />
+        </div>
+    )
+}
+export default ImageShow
---
+++ src/components/SearchBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function SearchBar() {
+    return (
+        <div>
+            <input className="input is-primary" type="text" placeholder="Primary input" />
+        </div>
+    )
+}
+export default SearchBar
